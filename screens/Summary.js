f1b477b95c7fe9978fa3e6e17e66a2f04857f724
Steven Pong <115668671+steven-pong@users.noreply.github.com>
2024-04-15 19:59:09 -0400

    Update Summary.js

--- screens/Summary.js
+++ screens/Summary.js
@@ -18,40 +18,46 @@ import styles from "../styles";
 <Text style={styles.in}>INCOME</Text>
 
 <Text style={styles.in}>Employment</Text>
-<Text style={styles.out}>$0.00</Text>
+<Text style={styles.out}>${income}</Text>
 <Text style={styles.in}>Self-Employment</Text>
-<Text style={styles.out}>$0.00</Text>
+<Text style={styles.out}>${sincomee</Text>
 <Text style={styles.in}>Investment</Text>
-<Text style={styles.out}>$0.00</Text>
+<Text style={styles.out}>${investment}</Text>
 <Text style={styles.in}>Pension</Text>
-<Text style={styles.out}>$0.00</Text>
+<Text style={styles.out}>${pension}</Text>
 <Text style={styles.in}>Public Benefits</Text>
-<Text style={styles.out}>$0.00</Text>
+<Text style={styles.out}>${govIncome}</Text>
 <Text style={styles.in}>Capital Gain (Loss)</Text>
-<Text style={styles.out}>$0.00</Text>
+<Text style={styles.out}>${capital}</Text>
 
 <Text style={styles.in}>Total</Text>
-<Text style={styles.out}>$0.00</Text>
+<Text style={styles.out}>${incomeTotal}</Text>
 
 <Text style={styles.in}>DEDUCTIONS</Text>
 
 <Text style={styles.in}>Business Expenses</Text>
-<Text style={styles.out}>$0.00</Text>
+<Text style={styles.out}>${expenses}</Text>
 <Text style={styles.in}>Charity Donation</Text>
-<Text style={styles.out}>$0.00</Text>
+<Text style={styles.out}>${donation}</Text>
 <Text style={styles.in}>Other Deductions</Text>
-<Text style={styles.out}>$0.00</Text>
+<Text style={styles.out}>${other}</Text>
 
 <Text style={styles.in}>Total</Text>
-<Text style={styles.out}>$0.00</Text>
+<Text style={styles.out}>${deductionTotal}</Text>
 
+var net = incomeTotal - deductionTotal;
+
+var perc = 0;            
+                           
 <Text style={styles.in}>NET TAXABLE</Text>
-<Text style={styles.out}>$0.00</Text>
+<Text style={styles.out}>${net}</Text>
 <Text style={styles.in}>Bracket</Text>
-<Text style={styles.out}>0%</Text>
+<Text style={styles.out}>{perc}%</Text>
 
+var amt = net * perc;
+                           
 <Text style={styles.in}>DEBIT (CREDIT)</Text>
-<Text style={styles.out}>$0.00</Text>
+<Text style={styles.out}>${amt}</Text>
 
 const styles = StyleSheet.create({
 
