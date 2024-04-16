23b0d79d596fd754729064562bca2406072afae0
Steven Pong <115668671+steven-pong@users.noreply.github.com>
2024-04-15 21:44:27 -0400

    Update Summary.js

--- screens/Summary.js
+++ screens/Summary.js
@@ -12,70 +12,75 @@ import SecondaryButton from "../components/SecondaryButton";
 import CustomButton from "../components/CustomButton";
 import styles from "../styles";
 
+const Summary = () => {
+ 
+ return (
+ 
+ <Text style={styles.title}>Summary</Text>
+ 
+ <Text style={styles.in}>INCOME</Text>
+ 
+ <Text style={styles.in}>Employment</Text>
+ <Text style={styles.out}>${user.income.employmentIncome}</Text>
+ <Text style={styles.in}>Self-Employment</Text>
+ <Text style={styles.out}>${user.income.selfemploymentIncome}</Text>
+ <Text style={styles.in}>Investment</Text>
+ <Text style={styles.out}>${user.income.investmentincome}</Text>
+ <Text style={styles.in}>Pension</Text>
+ <Text style={styles.out}>${user.income.pensionIncome + user.income.rrspIncome}</Text>
+ <Text style={styles.in}>Public Benefits</Text>
+ <Text style={styles.out}>${user.income.governmentBenefits}</Text>
+ <Text style={styles.in}>Capital Gain (Loss)</Text>
+ <Text style={styles.out}>${capital}</Text>
+ 
+ <Text style={styles.in}>Total</Text>
+ <Text style={styles.out}>${incomeTotal}</Text>
+ 
+ <Text style={styles.in}>DEDUCTIONS</Text>
+ 
+ <Text style={styles.in}>Business Expenses</Text>
+ <Text style={styles.out}>${expenses}</Text>
+ <Text style={styles.in}>Charity Donation</Text>
+ <Text style={styles.out}>${user.otherInformation.charitableDonations}</Text>
+ <Text style={styles.in}>Other Deductions</Text>
+ <Text style={styles.out}>${other}</Text>
+ 
+ <Text style={styles.in}>Total</Text>
+ <Text style={styles.out}>${deductionTotal}</Text>
+ 
+ var net = incomeTotal - deductionTotal;
+ 
+ var perc = 0;            
+                            
+ <Text style={styles.in}>NET TAXABLE</Text>
+ <Text style={styles.out}>${net}</Text>
+ <Text style={styles.in}>Bracket</Text>
+ <Text style={styles.out}>{perc}%</Text>
+ 
+ var amt = net * perc;
+                            
+ <Text style={styles.in}>DEBIT (CREDIT)</Text>
+ <Text style={styles.out}>${amt}</Text>
+ 
+ const styles = StyleSheet.create({
+ 
+  title: {
+   fontSize: 34,
+   fontWeight: "bold",
+   textAlign: "left",
+  },
+ 
+  in: {
+   fontSize: 18,
+   textAlign: "left",
+  }
+  out: {
+   fontSize: 18,
+   textAlign: "right",
+  }
+ 
+ });
 
-<Text style={styles.title}>Summary</Text>
-
-<Text style={styles.in}>INCOME</Text>
-
-<Text style={styles.in}>Employment</Text>
-<Text style={styles.out}>${income}</Text>
-<Text style={styles.in}>Self-Employment</Text>
-<Text style={styles.out}>${sincomee</Text>
-<Text style={styles.in}>Investment</Text>
-<Text style={styles.out}>${investment}</Text>
-<Text style={styles.in}>Pension</Text>
-<Text style={styles.out}>${pension}</Text>
-<Text style={styles.in}>Public Benefits</Text>
-<Text style={styles.out}>${govIncome}</Text>
-<Text style={styles.in}>Capital Gain (Loss)</Text>
-<Text style={styles.out}>${capital}</Text>
-
-<Text style={styles.in}>Total</Text>
-<Text style={styles.out}>${incomeTotal}</Text>
-
-<Text style={styles.in}>DEDUCTIONS</Text>
-
-<Text style={styles.in}>Business Expenses</Text>
-<Text style={styles.out}>${expenses}</Text>
-<Text style={styles.in}>Charity Donation</Text>
-<Text style={styles.out}>${donation}</Text>
-<Text style={styles.in}>Other Deductions</Text>
-<Text style={styles.out}>${other}</Text>
-
-<Text style={styles.in}>Total</Text>
-<Text style={styles.out}>${deductionTotal}</Text>
-
-var net = incomeTotal - deductionTotal;
-
-var perc = 0;            
-                           
-<Text style={styles.in}>NET TAXABLE</Text>
-<Text style={styles.out}>${net}</Text>
-<Text style={styles.in}>Bracket</Text>
-<Text style={styles.out}>{perc}%</Text>
-
-var amt = net * perc;
-                           
-<Text style={styles.in}>DEBIT (CREDIT)</Text>
-<Text style={styles.out}>${amt}</Text>
-
-const styles = StyleSheet.create({
-
- title: {
-  fontSize: 34,
-  fontWeight: "bold",
-  textAlign: "left",
- },
-
- in: {
-  fontSize: 18,
-  textAlign: "left",
- }
- out: {
-  fontSize: 18,
-  textAlign: "right",
- }
-
-});
+};
 
 export default Summary;
